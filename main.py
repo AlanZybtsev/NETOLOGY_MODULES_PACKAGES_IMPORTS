a87056109036d803d5ca2e2db9661f97ed267f26
Alan <alan.zybtsev@mail.ru>
2023-07-04 19:16:15 +0400

    first version

--- main.py
+++ main.py
@@ -2,13 +2,8 @@ import sys
 import datetime
 
 if __name__ == '__main__':
-    salary_way = 'B:/_PYTHON_/НЕТОЛОГИЯ/pd-77/05_Профессиональная_работа_с_Python/01_Модули, пакеты, импорты в Python/ДЗ/application'
-    sys.path.insert(1, salary_way)
-    from salary import calculate_salary
-
-    people_way = 'B:/_PYTHON_/НЕТОЛОГИЯ/pd-77/05_Профессиональная_работа_с_Python/01_Модули, пакеты, импорты в Python/ДЗ/application/db'
-    sys.path.insert(1, people_way)
-    import people
+    from application_ import salary
+    from application_.db import people
 
     now = datetime.datetime.now()
     print(f'Today\'s date - {now.date()}')
